e60b33bc381101c819cba9d0f2456a8d636739f9
Lucas <lucas@tempus.solutions>
2019-10-15 21:30:08 -0300

    Ambiente e Conceitos 03 - Insomnia e melhorias no GET users

--- modulo01/index.js
+++ modulo01/index.js
@@ -14,12 +14,14 @@ app.get('/testeQuery', (req,res) => {
     return res.json({message: `Olá Sr. ${nome}!` });
 });
 
+const users = ['Diego', 'Lucas', 'Robertinho'];
 // ROUTE PARAM  -> user/editar/1 PUT E DELETE (GERALMENTE ID'S)
-app.get('/users/:id', (req,res) => {
-    const { id } = req.params;
+app.get('/users/:index', (req,res) => {
+    const { index } = req.params;
 
-    return res.json({message: `Buscando o usuário com o ID ${id}.`});
+    return res.json({message: `Buscando o usuário ${users[index]}.`});
 });
+
 
 
 /*
