c9ffadc21c73456a0fe20b1e70d62c63580d1ba6
Lucas <lucas@tempus.solutions>
2019-10-15 21:21:27 -0300

    Ambiente e Conceitos 02 - Query e Route params

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,8 +4,23 @@ const app = express();
 
 /*
     ROTAS
+    BODY PARAMS  -> {nome: Lucas}
 */
-app.get('/teste', (req,res) => res.json({message: 'Hello World!' }));
+
+// QUERY PARAMS -> ?pagina=1 (Filtros e informações adicionais)
+app.get('/testeQuery', (req,res) => {
+    const { nome } = req.query; //?nome=Lucas
+
+    return res.json({message: `Olá Sr. ${nome}!` });
+});
+
+// ROUTE PARAM  -> user/editar/1 PUT E DELETE (GERALMENTE ID'S)
+app.get('/users/:id', (req,res) => {
+    const { id } = req.params;
+
+    return res.json({message: `Buscando o usuário com o ID ${id}.`});
+});
+
 
 /*
     Porta
