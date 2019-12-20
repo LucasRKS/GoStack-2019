c79576a661a537ec12b3d60f760caaa9dc4ede4a
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-12-20 13:47:01 -0300

    img

--- modulo06 REACT/src/App.js
+++ modulo06 REACT/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import fruits from './assets/download (1).png';
+
 function App() {
-  return <h1>Hello World!</h1>
+  return <img src={fruits} />
 }
 
 export default App;
