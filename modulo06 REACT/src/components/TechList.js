dd653da5985a8796091e7ee2a54b86b333fb2999
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2020-01-06 20:53:24 -0300

    Ciclo de vida de um componente

--- modulo06 REACT/src/components/TechList.js
+++ modulo06 REACT/src/components/TechList.js
@@ -5,12 +5,24 @@ class TechList extends Component {
   state = {
     newTech : '',
     techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native',
+      
     ]
   };
 
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if(techs) {
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+  
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
   }
