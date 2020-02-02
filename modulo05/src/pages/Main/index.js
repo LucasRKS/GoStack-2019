6f7823dcc0c92f4ef4394bb90fdc9bc9b828c97e
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2020-02-02 14:12:31 -0300

    página de repositório

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
