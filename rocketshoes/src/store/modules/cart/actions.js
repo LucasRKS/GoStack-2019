79cb03981430a59f5411d5f8cb5444b0b2621bed
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2020-02-17 14:11:41 -0300

    Rocketshoes -> Melhoria na nomenclatura para o reactotron

--- rocketshoes/src/store/modules/cart/actions.js
+++ rocketshoes/src/store/modules/cart/actions.js
@@ -1,7 +1,7 @@
 export function addToCart(product) {
-  return { type: 'ADD_TO_CART', product }
+  return { type: '@cart/ADD', product }
 }
 
 export function removeFromCart(id) {
-  return { type: 'REMOVE_FROM_CART', id }
+  return { type: '@cart/REMOVE', id }
 }
